ed8a746dcc8cb22421a89439d159f01d20257eca
Yulong Niu <yulong.niu@hotmail.com>
2019-10-18 11:04:17 +0200

    add pair-end data

--- RNA-Seq_scripts/download_SRA.sh
+++ RNA-Seq_scripts/download_SRA.sh
@@ -6,6 +6,8 @@ SRATOOLS_PATH=/extDisk1/Biotools/sratoolkit.2.10.0-centos_linux64/bin
 
 RAW_PATH=/extDisk1/RESEARCH/MPIPZ_SPP_workshop/RNA_Seq_raw_data/
 
+CORENUM=10
+
 cd ${RAW_PATH}
 
 ##~~~~~~~~~~~~~~~~~~~~single-end~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -19,7 +21,7 @@ SRAName=("Col0_s_rep1" "Col0_s_rep2" "Col0_s_rep3"
          "MeJA_s_rep1" "MeJA_s_rep2" "MeJA_s_rep3"
          "flg22_s_rep1" "flg22_s_rep2" "flg22_s_rep3")
 
-for i in {0..8}; do
+for i in {1..3}; do
 
     ${SRATOOLS_PATH}/fasterq-dump ${SRARaw[i]} -e 12 \
                     -O ${RAW_PATH}
@@ -40,13 +42,17 @@ SRARaw=("SRR7865799" "SRR7865800" "SRR7865801"
 SRAName=("Col0_p_rep1" "Col0_p_rep2" "Col0_p_rep3"
          "flg22_p_rep1" "flg22_p_rep2" "flg22_p_rep3")
 
-for i in {0..5}; do
-    ${SRATOOLS_PATH}/fasterq-dump ${SRARaw[i]} -e 12 \
+for i in {1..6}; do
+    ${SRATOOLS_PATH}/fasterq-dump ${SRARaw[i]} \
+                    -p \
+                    -e ${CORENUM} \
                     -O ${RAW_PATH}
 
-    # ${BIN_PATH}/gzip ${SRARaw[i]}.fastq
+    ${BIN_PATH}/gzip ${SRARaw[i]}_1.fastq
+    ${BIN_PATH}/gzip ${SRARaw[i]}_2.fastq
 
-    # ${BIN_PATH}/mv ${SRARaw[i]}.fastq.gz ${SRAName[i]}.fq.gz
+    ${BIN_PATH}/mv ${SRARaw[i]}_1.fastq.gz ${SRAName[i]}_R1.fq.gz
+    ${BIN_PATH}/mv ${SRARaw[i]}_2.fastq.gz ${SRAName[i]}_R2.fq.gz
 done
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
